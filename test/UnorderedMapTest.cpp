336fa7cfe46309c94016b9de3f7e5640ace80d80
Pedro <mail@rieramartorell.cat>
2023-04-30 12:51:36 +0200

    TDD style: failing test

--- test/UnorderedMapTest.cpp
+++ test/UnorderedMapTest.cpp
@@ -2,9 +2,22 @@
 
 #include "my/unordered_map.h"
 
+using SimpleMap = my::unordered_map<int, int>;
+
 TEST(UnorderedMapTest, NewMapIsEmpty) {
-    my::unordered_map<int, int> emptyMap {};
+    SimpleMap emptyMap {};
 
     EXPECT_TRUE(emptyMap.empty());
     EXPECT_EQ(emptyMap.size(), 0);
+}
+
+TEST(UnorderedMapTest, InsertSingleElementSuccessful) {
+    SimpleMap emptyMap {};
+
+    auto kvPair = SimpleMap::value_type {1, 2};
+    const bool insertionResult = emptyMap.insert(kvPair);
+
+    EXPECT_FALSE(emptyMap.empty());
+    EXPECT_TRUE(insertionResult);
+    EXPECT_EQ(emptyMap.size(), 1);
 }
